cd3c29ad3c4eee27912674517f9743bda0fa928e
小程序 <51513719+Code-keys@users.noreply.github.com>
2021-06-17 21:16:40 +0800

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,6 @@
+#include "__init__.h"
+#include "cxxopts.hpp"
+
+int main(){
+    printf("    this is test frame! \nn\n");
+}
---
+++ train.cpp
@@ -0,0 +1,6 @@
+#include "__init__.h"
+#include "cxxopts.hpp"
+
+int main(){
+    printf("    this is train frame! \n\n");
+}
